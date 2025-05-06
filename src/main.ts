9d576746d2642d7c6f52401377f285e0e8c279a2
shruti.groovy <shrusti.kapatel@groovyweb.co>
2025-05-06 15:02:56 +0530

    ✨ :Add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,40 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './interceptor/response.interceptor';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.enableCors(); // This allows ALL origins
 
   const configService = app.get(ConfigService); // 💉 inject ConfigService
   const port = configService.get<number>('PORT') || 3000;
+  const swaggerEnable = configService.get<string>('ENABLE_SWAGGER');
+
+  if (swaggerEnable === 'true') {
+    const config = new DocumentBuilder()
+      .setTitle('Social API')
+      .setDescription('Saayam Social API List')
+      .setVersion('1.0')
+      .addBearerAuth(
+        {
+          // I was also testing it without prefix 'Bearer ' before the JWT
+          description: `[just text field] Please enter token in following format: Bearer <JWT>`,
+          name: 'Authorization',
+          bearerFormat: 'Bearer', // I`ve tested not to use this field, but the result was the same
+          scheme: 'Bearer',
+          type: 'http', // I`ve attempted type: 'apiKey' too
+          in: 'Header',
+        },
+        'access-token', // This name here is important for matching up with @ApiBearerAuth() in your controller!
+      )
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(port);
 }
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,34 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   _id: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   firstName: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   lastName: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   displayName: string;
 
+  @ApiProperty()
   @IsEmail()
   @IsString()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   phoneCode: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   phoneNumber: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   type: string;
